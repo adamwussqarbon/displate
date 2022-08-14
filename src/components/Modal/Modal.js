072106ac6874c56eca988c73cec1554a90ebd27f
Adam Wuss <adamwusspl@gmail.com>
2022-08-14 17:17:50 +0200

    feat: add Modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+export function Modal() {
+  return (
+    <div className="modal" tabIndex="-1">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">
+              Random photo of this breed
+            </h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+          </div>
+          <div className="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Next photo
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
